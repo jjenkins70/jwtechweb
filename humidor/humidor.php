985a1cad07fbac751677df5ed82379ee762227c2
EC2 Default User <ec2-user@ip-10-0-1-189.ec2.internal>
2015-01-05 19:38:57 -0500

    humidor test site

---
+++ humidor/humidor.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "jwpublicdb.cwsolm1nl2vx.us-east-1.rds.amazonaws.com";
+$username = "jw_tech_pub";
+$password = "Golfgod70!";
+$dbname = "cigar_monitor";
+
+// Create Connection
+$mysqli = new mysqli($servername,$username,$password,$dbname);
+// Check connection
+if (mysqli_connect_error()) {
+  die("connection failed: ". mysqli_connect_error());
+}
+
+$sql = "SELECT date, temp, hum from TempHumid order by date desc";
+$result = $mysqli->query($sql);
+
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>Date</th><th>Tempature</th><th>Humidity</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["date"]."</td><td>".$row["temp"]."</td><td>".$row["hum"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$mysqli->close();
+?>
+
